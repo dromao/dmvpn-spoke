f98f1cd4c360a4a503067158d4792646e3a659ab
Daniel Romão <dromao@Daniels-Mac-mini.local>
2015-06-23 20:30:00 +0200

    Changes and fixes

--- spoke-setup.sh
+++ spoke-setup.sh
@@ -2,43 +2,69 @@
 # Intial setup script
 # Tested with Tails 1.4
 
+# Configure serial interface
+stty -F /dev/ttyUSB0 19200 clocal cs8 -cstopb -parenb
+
+#Install rng-tools and configure RNG
 apt-get update
 apt-get install rng-tools -y
 
-echo "HRNGDEVICE=/dev/ttyUSB0" > /etc/default/rng-tools
+cp files/configuration/rng-tools /etc/default/rng-tools
 service rng-tools restart
 
+# Download build tools and dependecies
+apt-get install build-essential libc-ares-dev pkg-config -y
+
+# Download and compile OpenNHRP
+wget http://downloads.sourceforge.net/project/opennhrp/opennhrp/opennhrp-0.14.1.tar.bz2
+tar xf opennhrp-0.14.1.tar.bz2
+cd opennhrp-0.14.1
+make
+cd ..
+rm opennhrp-0.14.1.tar.bz2
+
 # Configure Hub
-echo -e "\nInsert IP address and netmask of the DMVPN hub tunnel (ex. 10.255.255.1/24): "
-read TUNNEL
+echo -e "\nInsert IP address of the hub tunnel interface (ex. 10.255.255.1): \c"
+read TUNNEL_HUB_IP
 
-sed -i 's/to_fill_tunnel/$TUNNEL/g' files/configuration/opennhrp.conf
+sed -i "s/TUNNEL_HUB_IP/$TUNNEL_HUB_IP/g" files/configuration/opennhrp.conf
 
-echo -e "\nInsert IP address or FQDN of the server/router that is the DMVPN hub: "
+echo -e "\nInsert IP address of the server/router that is the DMVPN hub: \c"
 read HUB_IP
 
-sed -i 's/to_fill_hub/$HUB_IP/g' files/configuration/opennhrp.conf
+sed -i "s/HUB_IP/$HUB_IP/g" files/configuration/opennhrp.conf
 
 # Configure Spoke
-echo -e "\nInsert IP address and netmask for this spoke (ex. 10.255.255.10/24): "
-read SPOKE
+echo -e "\nInsert IP address for this spoke tunnel interface (ex. 10.255.255.10): \c"
+read TUNNEL_SPOKE_IP
 
-sed -i 's/to_fill_tunnel/$SPOKE/g' spoke-start.sh
+sed -i "s/TUNNEL_SPOKE_IP/$TUNNEL_SPOKE_IP/g" spoke-start.sh
+
+# Configure netmask
+echo -e "\nInsert netmask of the tunnel network (ex. 24): \c"
+read TUNNEL_NETMASK
+
+sed -i "s/TUNNEL_NETMASK/$TUNNEL_NETMASK/g" files/configuration/opennhrp.conf
+sed -i "s/TUNNEL_NETMASK/$TUNNEL_NETMASK/g" spoke-start.sh
 
 #Configure secret
-echo -e "\nInsert DMVPN authentication string: "
-read AUTH
+echo -e "\nInsert DMVPN authentication string: \c"
+read SECRET
 
-sed -i 's/secret/$AUTH/g' files/configuration/opennhrp.conf
+sed -i "s/SECRET/$SECRET/g" files/configuration/opennhrp.conf
 
 # Create keys
-echo "A key and certificate will be created now for this spoke"
+echo -e "\nA key and certificate will be created now for this spoke\n"
 
-openssl genrsa -des3 -out files/certs/key_encrypted.key 4096
-openssl rsa -in files/certs/key_encrypted.key -out files/certs/key.pem
-openssl req -new -key files/certs/key.pem -out files/certs/cert.csr
+openssl genrsa -des3 -out key_encrypted.key 4096
+openssl rsa -in key_encrypted.key -out key.pem
+openssl req -new -key key.pem -out cert.csr
+
+mv key.pem files/certs/
+mv cert.csr files/certs
+rm key_encrypted.key
 
 echo -e "\nEnd of configuration!\n"
 echo "The file ./files/certs/cert.csr will need to be signed using the Root CA key and certificate."
 echo "The file name of the certificate should be: cert.pem"
-echo "The certificate of the CA will also have to be included in the certs directory. The file should be named ca.pem"
+echo -e "The certificate of the CA will also have to be included in the certs directory. The file should be named ca.pem\n"
--- spoke-start.sh
+++ spoke-start.sh
@@ -2,36 +2,48 @@
 # Deployment script
 # Tested with Tails 1.4
 
+# Configure serial interface
+stty -F /dev/ttyUSB0 19200 clocal cs8 -cstopb -parenb
+
+#Install rng-tools and configure RNG
 apt-get update
 apt-get install rng-tools -y
 
-echo "HRNGDEVICE=/dev/ttyUSB0" > /etc/default/rng-tools
+cp files/configuration/rng-tools /etc/default/rng-tools
+service rng-tools restart
 
-# Install OpenNHRP package (Dependencies are automatically installed as well)
-dpkg -i files/opennhrp/opennhrp_0.14.1-1_amd64.deb
+# Install dependencies and build tools
+apt-get install racoon ipsec-tools build-essential libc-ares-dev pkg-config -y
+
+# Install OpenNHRP
+cd opennhrp-0.14.1
+make install
+cd ..
 
 # Copy configuration files
 cp files/configuration/opennhrp.conf /etc/opennhrp/opennhrp.conf
 cp files/configuration/racoon.conf /etc/racoon/racoon.conf
 cp files/configuration/ipsec-tools.conf /etc/ipsec-tools.conf
+cp files/configuration/ferm.conf /etc/ferm/ferm.conf
 
 # Copy keys' directory
-cp -r files/certs /etc/racoon/certs
+cp files/certs/* /etc/racoon/certs/
 
 # Load GRE kernel module
 modprobe ip_gre
 
 # Create GRE interface
 ip tunnel add gre1 mode gre key 1234 ttl 64
-ip addr add to_fill_tunnel dev gre1
+ip addr add 10.255.255.10/24 dev gre1
 ip link set gre1 up
 
 # Restart services
 service rng-tools restart
 service racoon restart
 service setkey restart
+service ferm restart
 
 # Start OpenNHRP
 /usr/sbin/opennhrp -d
 
-echo "Completed!"
+echo -e "\nCompleted!\n"
